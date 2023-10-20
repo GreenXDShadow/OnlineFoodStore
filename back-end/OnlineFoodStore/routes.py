f469fd32160af4fd80c90d89de1892eb25c1b8cf
GreenXDShadow <NickDValen@gmail.com>
2023-10-20 01:59:04 -0700

    All replit files moved to Flask. Will work on integrating back end tomorrow. Might make another commit to remove the front end folder

--- back-end/OnlineFoodStore/routes.py
+++ back-end/OnlineFoodStore/routes.py
@@ -5,6 +5,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  # do something with the request data
-  return render_template('index.html', name= index)
+    return render_template('index.html')
 
+@app.route('/fruit')
+def fruit():
+    return render_template('fruit.html')
+
+@app.route('/vegetable')
+def vegetable():
+    return render_template('vegetable.html')
+
+@app.route('/dairy')
+def dairy():
+    return render_template('dairy.html')
+
+@app.route('/meat')
+def meat():
+    return render_template('meat.html')
+
+@app.route('/frozen')
+def frozen():
+    return render_template('frozen.html')
+
+@app.route('/beverages')
+def beverages():
+    return render_template('beverages.html')
+
+@app.route('/adminlogin')
+def adminlogin():
+    return render_template('adminlogin.html')
